15cb928b0b3f2163f6ef4a86144c28b847c435f6
ssb-nvj <nvj@ssb.no>
2025-03-20 14:30:08 +0000

    update profiling results

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 # Dapla Lab kladdeark
 
-Profiling results from 'delomaten'. Pseudonymizing two columns
-using DAEAD.
+## Profiling pseudonymization
+Profiling pseudonymization through dapla-toolbelt-pseudo.
 
-| Rows         | Max memory usage | Real time  | User time |   |
-|--------------|------------------|------------|-----------|---|
-| 1 million    | ~1GB             | 3m31s      |           |   |
-| 1.5 million  | ~1.1GiB          | 4m43s      |           |   |
-| 2 million    | ~1.7GiB          | 8m14s      | 4m23s     |   |
+TODO: Run the tests several times and normalize the results.
 
-Different profiling results:
-![image](./profiling_results/mem_2m_rows.png)
+### DAEAD
+Profiling pseudonymizing two columns using DAEAD.
+The dataset here is randomly generated ascii strings of length 20.
+
+| Rows         | Max memory usage | Real time  | User time | Profiling result                                      |
+|--------------|------------------|------------|-----------|-------------------------------------------------------|
+| 1 million    | ~1GB             | 3m31s      |           |                                                       |
+| 1.5 million  | ~1.1GiB          | 4m43s      |           |                                                       |
+| 2 million    | ~1.7GiB          | 8m14s      | 4m23s     | ![image](./profiling_results/prof_default_2m_rows.png)|
+
+
+### SID mapping + FPE (format preserving)
+Profiling pseudonymizing two columns using SID mapping + FPE.
+The dataset here is 3 test 'fnr's that are repeated to form a column of `n` rows.
+
+| Rows         | Max memory usage | Real time  | User time | Profiling result                                    |
+|--------------|------------------|------------|-----------|-----------------------------------------------------|
+| 1 million    | ~0.73GiB         | 3m12s      | 1m43s     | ![image](./profiling_results/prof_sid_1m_rows.png)  |
+| 1.5 million  | ~0.78GiB         | 4m42       | 2m23s     | ![image](./profiling_results/prof_sid_1.5m_rows.png)|
+| 2 million    | ~1.1GiB          | 6m14s      | 3m10s     | ![image](./profiling_results/prof_sid_2m_rows.png)  |
+| 3 million    | ~1.6GiB          | 8m53s      | 4m27s     | ![image](./profiling_results/prof_sid_3m_rows.png)  |
+| 4 million    | ~2.25GiB         | 15m4s      | 7m2s      | ![image](./profiling_results/prof_sid_4m_rows.png)  |
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from multiprocessing import Pool, cpu_count
 from math import floor
 from dapla_pseudo import Pseudonymize
 import sys
-import os
+from typing import Literal
+from itertools import cycle
 
 def random_string(length: int = 10) -> str:
     """Generate a single random alphanumeric string."""
@@ -25,33 +26,47 @@ def gen_random_ints(n: int) -> list[int]:
     with Pool(processes=cpu_count()) as pool:
         return pool.starmap(func=gen_int, iterable=[()] * n)
 
-def generate_polars_dataframe(n: int) -> pl.DataFrame:
+def generate_polars_dataframe(n: int, pseudo_type: Literal["sid", "default"]) -> pl.DataFrame:
+    fnrs = cycle(["11854898347", "01839899544", "16910599481"])
+    test_fnrs: list[str] | None = [next(fnrs) for _ in range(n)] if pseudo_type == "sid" else None 
     return pl.DataFrame({
-        "fornavn": gen_random_strings(n),
-        "etternavn": gen_random_strings(n),
+        "fornavn": test_fnrs if pseudo_type == "sid" else gen_random_strings(n),
+        "etternavn": test_fnrs if pseudo_type == "sid" else gen_random_strings(n),
         "alder": gen_random_ints(n)
     })
 
-def gen_large_parquet(n: int):
-    path = f"/home/onyxia/work/kladd/output/{n}_rows.parquet"
+def gen_large_parquet(n: int, pseudo_type: Literal["sid", "default"]):
+    path = f"/home/onyxia/work/kladd/input/{pseudo_type}_{n}_rows.parquet"
     print(f"Generating parquet file with {n} rows")
-    df = generate_polars_dataframe(n)
+    df = generate_polars_dataframe(n, pseudo_type)
+    print(df)
     df.write_parquet(path)
     print(f"File written to {path}")
 
-def main(n: int):
-    path_read = f"/home/onyxia/work/kladd/output/{n}_rows.parquet"
-    path_write = f"/home/onyxia/work/kladd/output/{n}_rows_pseudonymized.parquet"
+def main(n: int, pseudo_type: Literal["sid", "default"]):
+    path_read = f"/home/onyxia/work/kladd/input/{pseudo_type}_{n}_rows.parquet"
+    path_write = f"/home/onyxia/work/kladd/output/{pseudo_type}_{n}_rows_pseudonymized.parquet"
     df = pl.read_parquet(path_read)
     print(f"Pseudonymizing DataFrame with shape f{df.shape}...")
-    df = (
-      Pseudonymize
-        .from_polars(df)
-        .on_fields("fornavn","etternavn")
-        .with_default_encryption()
-        .run(timeout = 20 * 60)
-        .to_polars()
-    )
+    if pseudo_type == "sid":
+        df = (
+            Pseudonymize
+            .from_polars(df)
+            .on_fields("fornavn","etternavn")
+            .with_stable_id()
+            .run(timeout = 20 * 60)
+            .to_polars()
+        )
+    else:
+        df = (
+            Pseudonymize
+            .from_polars(df)
+            .on_fields("fornavn","etternavn")
+            .with_default_encryption()
+            .run(timeout = 20 * 60)
+            .to_polars()
+        )
+
     print(df)
     df.write_parquet(path_write)
     print(f"File written to {path_write}")
@@ -59,12 +74,18 @@ def main(n: int):
 if __name__ == "__main__":
     args = sys.argv[1:]
     match args:
-        case ["gen", i]:
+        case ["gen", t, i]:
             n = floor(float(i) * 1_000_000)
-            gen_large_parquet(n)
-        case ["write", i]:
+            if t != "sid" and t != "default":
+                print("Invalid pseudo_type argument")
+                sys.exit(1)
+            gen_large_parquet(n, t)
+        case ["write", t, i]:
             n = floor(float(i) * 1_000_000)
-            main(n)
+            if t != "sid" and t != "default":
+                print("Invalid pseudo_type argument")
+                sys.exit(1)
+            main(n, t)
         case _:
             print("Invalid argument passed")
             sys.exit(1)
